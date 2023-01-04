0211a977aa8605ce48ff8d0b87a3bb428d43f80a
Noemi1 <calmeida.no@gmail.com>
2023-01-04 11:01:46 -0300

    contact form section

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
     AOS.init();
-    
-    let headerHeight = $('.header').hasClass('active') ? 60 : 80;
+        let headerHeight = $('.header').hasClass('active') ? 60 : 80;
 
     // header active
     $(window).on('scroll', function() {
@@ -22,6 +21,26 @@ $(document).ready(function(){
         slidesToShow: 1,
         slidesToScroll: 1,
     });
+    $('#telefoneCelular').mask('(00) 0000-00000', {
+        onKeyPress: function(value, e, field, options) {
+            var masks = ['(00) 0000-00000', '(00) 0.0000-0000'];
+            var mask = (value.length>=15) ? masks[1] : masks[0];
+            $(field).mask(mask, options);
+        }
+    });
+    // form validation
+    $.extend($.validator.messages, {
+        required: "Esse campo é obrigatório.",
+        email: "E-mail inválido."
+    });
+    let validator = $('.contato_form').validate({
+        submitHandler: function(form) {
+
+        }
+    });
+    $('.contato_form').find('.form-control').on('blur', function() {
+        validator.element($(this))
+    })
 
     // button scroll down
     $('.btn-scrow-down').on('click', function() {
